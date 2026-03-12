5b0fe5b1f46556fc94af6d4c77682bc43e38f640
Josie <vmutas444@outlook.com>
2026-03-12 06:06:29 +0100

    jog now works like it should
    just stole the american audio raduios 2000 js file and and wokred since its both player are made by the same company

--- Stadnon-CMP-800.js
+++ Stadnon-CMP-800.js
@@ -1,41 +1,264 @@
-function CMP800() {
-    // constructor function (can be empty or init stuff)
+/**
+ * American Audio Radius 2000 controller script v1.10.0
+ * Written by Markus Steinbauer 2011
+ *
+ * Special Thanks to the Programmer of the Behringer BCD 3000 Mapping.
+ * The Mapping for the Jog Wheel and Scratch function is great.
+ **/
+
+CMP800 = new Controller();
+
+CMP800.currentDeck = function (group) {
+    if (group == "[Channel1]")
+        return 0;
+    else if (group == "[Channel2]")
+        return 1;
+    print("Invalid group : " + group);
+    return -1; // error
 }
 
-// The button that enables/disables scratching
-CMP800.wheelTouch = function (channel, control, value, status, group) {
-    var deck = script.deckFromGroup(group);
-    if ((status & 0xF0) === 0x90) {    // If button down
-        var alpha = 1.0/8;
-        var beta = alpha/16;
-        engine.scratchEnable(deck, 128, 33+1/3, alpha, beta);
-    } else {    // If button up
-        engine.scratchDisable(deck);
+CMP800.currentDeck2 = function (group) {
+    if (group == "[Channel1]")
+        return "[Channel1]";
+    else if (group == "[Channel2]")
+        return "[Channel2]";
+
+    print("Invalid group : " + group);
+    return -1; // error
+}
+
+CMP800.debug = false;
+
+CMP800.escratch = [false, false];
+
+CMP800.UseAcceleration = true;
+
+CMP800.JogSensitivity = 0.5;
+
+CMP800.init = function (channel, control, value, status, group) {
+    midi.sendShortMsg(0x90,0x2A,0x00);
+    midi.sendShortMsg(0x90,0x0B,0x7F);
+    midi.sendShortMsg(0x90,0x05,0x7F);
+    midi.sendShortMsg(0x90,0x30,0x7F);
+    midi.sendShortMsg(0x90,0x04,0x7F);
+    midi.sendShortMsg(0x90,0x02,0x7F);
+    midi.sendShortMsg(0x90,0x06,0x00);
+    midi.sendShortMsg(0x90,0x16,0x7F);
+    midi.sendShortMsg(0x90,0x10,0x7F);
+    midi.sendShortMsg(0x90,0x0A,0x7F);
+    midi.sendShortMsg(0x90,0x22,0x7F);
+    midi.sendShortMsg(0x90,0x19,0x7F);
+    midi.sendShortMsg(0x90,0x1F,0x7F);
+    midi.sendShortMsg(0x90,0x31,0x7F);
+    midi.sendShortMsg(0x90,0x01,0x7F);
+}
+
+CMP800.shutdown = function () {
+    midi.sendShortMsg(0x90,0x2A,0x00);
+    midi.sendShortMsg(0x90,0x0B,0x00);
+    midi.sendShortMsg(0x90,0x05,0x00);
+    midi.sendShortMsg(0x90,0x30,0x00);
+    midi.sendShortMsg(0x90,0x04,0x00);
+    midi.sendShortMsg(0x90,0x02,0x00);
+    midi.sendShortMsg(0x90,0x06,0x00);
+    midi.sendShortMsg(0x90,0x16,0x00);
+    midi.sendShortMsg(0x90,0x10,0x00);
+    midi.sendShortMsg(0x90,0x0A,0x00);
+    midi.sendShortMsg(0x90,0x22,0x00);
+    midi.sendShortMsg(0x90,0x1E,0x00);
+    midi.sendShortMsg(0x90,0x19,0x00);
+    midi.sendShortMsg(0x90,0x1F,0x00);
+    midi.sendShortMsg(0x90,0x0C,0x00);
+    midi.sendShortMsg(0x90,0x31,0x00);
+    midi.sendShortMsg(0x90,0x32,0x00);
+    midi.sendShortMsg(0x90,0x33,0x00);
+    midi.sendShortMsg(0x90,0x01,0x00);
+}
+
+CMP800.trackSearch = function (channel, control, value, status, group) {
+    if (value == 0x41) {
+        engine.setValue("[Playlist]","SelectNextTrack",1);
+    }
+    if (value == 0x3F) {
+        engine.setValue("[Playlist]","SelectPrevTrack",1);
     }
 }
 
-// The wheel that actually controls the scratching
+CMP800.menuSearch = function (channel, control, value, status, group) {
+    if (value == 0x41) {
+        engine.setValue("[Playlist]","SelectNextPlaylist",1);
+    }
+    if (value == 0x3F) {
+        engine.setValue("[Playlist]","SelectPrevPlaylist",1);
+    }
+}
+
+CMP800.play = function (channel, control, value, status, group) {
+    var currentlyPlaying = engine.getValue(CMP800.currentDeck2(group),"play");
+    if ((currentlyPlaying == 1) & (value == 0x00)) {
+        engine.setValue(CMP800.currentDeck2(group),"play",0);
+        midi.sendShortMsg(0x90,0x2A,0x00);
+    }
+    if ((currentlyPlaying == 0) & (value == 0x00)) {
+        engine.setValue(CMP800.currentDeck2(group),"play",1);
+        midi.sendShortMsg(0x90,0x2A,0x7F);
+        midi.sendShortMsg(0x90,0x30,0x00);
+    }
+}
+
+CMP800.cue = function (channel, control, value, status, group) {
+    var currentlyPlaying = engine.getValue(CMP800.currentDeck2(group),"play");
+    if ((currentlyPlaying == 1) & (value == 0x7F)) {
+        engine.setValue(CMP800.currentDeck2(group),"cue_default",1);
+        midi.sendShortMsg(0x90,0x2A,0x00);
+        midi.sendShortMsg(0x90,0x30,0x7F);
+    }
+    else if ((currentlyPlaying == 0) & (value == 0x7F)) {
+        engine.setValue(CMP800.currentDeck2(group),"cue_default",1);
+        midi.sendShortMsg(0x90,0x30,0x7F);
+    }
+    else {
+        engine.setValue(CMP800.currentDeck2(group),"cue_default",0);
+        midi.sendShortMsg(0x90,0x30,0x7F);
+    }
+}
+
+CMP800.keylock = function (channel, control, value, status, group) {
+    var keylockStat = engine.getValue(CMP800.currentDeck2(group),"keylock");
+    if (value == 0x7F & keylockStat == 1) {
+        engine.setValue(CMP800.currentDeck2(group),"keylock",0);
+        midi.sendShortMsg(0x90,0x06,0x00);
+    }
+    if (value == 0x7F & keylockStat == 0) {
+        engine.setValue(CMP800.currentDeck2(group),"keylock",1);
+        midi.sendShortMsg(0x90,0x06,0x7F);
+    }
+}
+
+CMP800.pitchRateRange = function (channel, control, value, status, group) {
+    var pitchRateRange = engine.getValue(CMP800.currentDeck2(group),"rateRange")
+    if (value == 0x7F) {
+        switch (pitchRateRange) {
+            case 0.04:
+                engine.setValue(CMP800.currentDeck2(group),"rateRange",0.08);
+                midi.sendShortMsg(0x90,0x0C,0x00);
+                midi.sendShortMsg(0x90,0x31,0x7F);
+                midi.sendShortMsg(0x90,0x32,0x00);
+                midi.sendShortMsg(0x90,0x33,0x00);
+                midi.sendShortMsg(0x90,0x01,0x7F);
+                break;
+            case 0.08:
+                engine.setValue(CMP800.currentDeck2(group),"rateRange",0.16);
+                midi.sendShortMsg(0x90,0x0C,0x00);
+                midi.sendShortMsg(0x90,0x31,0x00);
+                midi.sendShortMsg(0x90,0x32,0x7F);
+                midi.sendShortMsg(0x90,0x33,0x00);
+                break;
+            case 0.16:
+                engine.setValue(CMP800.currentDeck2(group),"rateRange",1.00);
+                midi.sendShortMsg(0x90,0x0C,0x00);
+                midi.sendShortMsg(0x90,0x31,0x00);
+                midi.sendShortMsg(0x90,0x32,0x00);
+                midi.sendShortMsg(0x90,0x33,0x7F);
+                midi.sendShortMsg(0x90,0x01,0x7F);
+                break;
+            case 1.00:
+                engine.setValue(CMP800.currentDeck2(group),"rateRange",0.04);
+                midi.sendShortMsg(0x90,0x0C,0x7F);
+                midi.sendShortMsg(0x90,0x31,0x00);
+                midi.sendShortMsg(0x90,0x32,0x00);
+                midi.sendShortMsg(0x90,0x33,0x00);
+                midi.sendShortMsg(0x90,0x01,0x7F);
+                break;
+            default:
+                engine.setValue(CMP800.currentDeck2(group),"rateRange",0.16);
+                midi.sendShortMsg(0x90,0x0C,0x00);
+                midi.sendShortMsg(0x90,0x31,0x7F);
+                midi.sendShortMsg(0x90,0x32,0x00);
+                midi.sendShortMsg(0x90,0x33,0x00);
+                midi.sendShortMsg(0x90,0x01,0x7F);
+                break;
+        }
+    }
+}
+
+CMP800.pitchRate = function (channel, control, value, status, group) {
+    var pitchRate = engine.getValue(CMP800.currentDeck2(group),"rateRange");
+    if ((value == 0x7F) & (pitchRate != 0)) {
+        engine.setValue(CMP800.currentDeck2(group),"rateRange",0);
+        midi.sendShortMsg(0x90,0x01,0x00);
+        midi.sendShortMsg(0x90,0x0C,0x00);
+        midi.sendShortMsg(0x90,0x31,0x00);
+        midi.sendShortMsg(0x90,0x32,0x00);
+        midi.sendShortMsg(0x90,0x33,0x00);
+    }
+    if ((value == 0x7F) & (pitchRate == 0)) {
+        engine.setValue(CMP800.currentDeck2(group),"rateRange",0.08);
+        midi.sendShortMsg(0x90,0x01,0x7F);
+        midi.sendShortMsg(0x90,0x31,0x7F);
+    }
+}
+
+CMP800.flanger = function (channel, control, value, status, group) {
+    var flangerFX = engine.getValue(CMP800.currentDeck2(group),"flanger");
+    if ((value == 0x7F) & (flangerFX == 0)) {
+        engine.setValue(CMP800.currentDeck2(group),"flanger",1);
+        midi.sendShortMsg(0x90,0x2D,0x7F);
+    }
+    if ((value == 0x7F) & (flangerFX == 1)) {
+        engine.setValue(CMP800.currentDeck2(group),"flanger",0);
+        midi.sendShortMsg(0x90,0x2D,0x00);
+    }
+
+}
+
+CMP800.flangerDelay = function (channel, control, value, status, group) {
+    var delayLevel = engine.getValue(CMP800.currentDeck2(group),"lfoDelay");
+    if (value == 0x41) {
+        engine.setValue("Flanger","lfoDelay",10000);
+    }
+    if (value == 0x3F){
+        engine.setValue("Flanger","lfoDelay",-10000);
+    }
+
+}
+
+
 CMP800.wheelTurn = function (channel, control, value, status, group) {
-    // --- Choose only one of the following!
-    
-    // A: For a control that centers on 0:
-    var newValue;
-    if (value < 64) {
-        newValue = value;
+    deck = CMP800.currentDeck(group);
+    if (CMP800.escratch[deck]) {
+        scratchValue = (value - 0x40);
+        engine.scratchTick(deck + 1, scratchValue);
     } else {
-        newValue = value - 128;
-    }
 
-    // Or: For a control that centers on 0x40 (64):
-    // var newValue = value - 64;
-    
-    // --- End choice
-    
-    // In either case, register the movement
-    var deck = script.deckFromGroup(group);
-    if (engine.isScratching(deck)) {
-        engine.scratchTick(deck, newValue); // Scratch!
-    } else {
-        engine.setValue(group, 'jog', newValue); // Pitch bend
+        jogValue = (value - 0x40) * CMP800.JogSensitivity;
+        engine.setValue(group, "jog", jogValue);
     }
 }
+
+CMP800.wheelTouch = function (channel, control, value, status, group) {
+    if (value == 0x7F) {
+        deck = CMP800.currentDeck(group);
+        CMP800.escratch[deck] = true;
+        print(CMP800.escratch[deck]);
+        engine.scratchEnable(deck + 1, 100, 330, 1.0/8, (1.0/8)/32);
+    }
+    else {
+        deck = CMP800.currentDeck(group);
+        CMP800.escratch[deck] = false;
+        engine.scratchDisable(deck + 1);
+        }
+}
+/*
+Not jet in config:
+Scratch Modi: (Normal/ Scratch / A.CUE Scratch)
+BPM TAP
+Memory
+Sample
+All Effects but Flanger (Because they are not available in Mixxx)
+Time
+SGL/CTN
+Parameter Buttons
+ADV.
+Folder Knob (Turning works, but pressing does not.)
+*/
